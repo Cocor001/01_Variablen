1a0794a1e977003ccb4c9d3a1805d09ed329bbcf
John Doe <mac2222.ai@gmail.com>
2021-11-18 12:11:15 +0100

    Dead

--- script_01.js
+++ script_01.js
@@ -1,2 +1,27 @@
 /***** Variablen 01 *******/
 
+/**
+ *  Block-Kommentar
+ */
+
+//Zeilen Kommentar
+
+//Konsolen-Ausgabe
+// console.log("Hello,World!");
+// //console.log(firstname); //nicht deklarierte War, ---> Fehler
+
+// /***** 02 Deklaration + Wertzuweisung */
+// let firstname; //Deklaration (Definition)
+// firstname= "Alex "; //Wertzuweisung
+// console.log(firstname); //Ausgabe
+
+// let familyname = "Van Hanglow"; // Deklaration + Wertzuweisung
+// console.log(familyname); //Ausgabe 
+// console.log(firstname +" "+ familyname);
+
+//***** 03 Deklaration + Wertzuweisung II *****/
+
+let firstname, familyname;
+firstname = prompt ("Bitte Vorname eingeben");
+familyname = prompt ("Bitte Nachname eingeben");
+console.log(firstname +" "+ familyname);
